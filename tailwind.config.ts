8c4aebf2ae7dafa297c31f043386c12e6e04f210
muffin9 <jinha6707@gmail.com>
2024-12-04 16:33:23 +0900

    chore: @tanstack/react-query set, font Y 너만을 비춤체 적용 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mainfont: ['var(--font-spotlight)'],
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
