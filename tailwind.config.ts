46eda9f6a1ccfe112bf7a7eec7ed3780462f3aeb
muffin9 <jinha6707@gmail.com>
2024-12-06 21:51:22 +0900

    chore: font size + font weight 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,13 +13,22 @@ export default {
       fontFamily: {
         mainfont: ['var(--font-spotlight)'],
       },
+      fontSize: {
+        title: ['30px', { lineHeight: '1.2', fontWeight: '700' }], // 큰 제목
+        xl: ['24px', { lineHeight: '1.3', fontWeight: '600' }], // 중간 제목
+        lg: ['20px', { lineHeight: '1.4', fontWeight: '400' }], // 본문 강조
+        md: ['16px', { lineHeight: '1.5', fontWeight: '400' }], // 일반 본문
+        sm: ['14px', { lineHeight: '1.6', fontWeight: '400' }], // 부가 설명
+        xs: ['12px', { lineHeight: '1.6', fontWeight: '400' }], // 세부 설명
+        caption: ['10px', { lineHeight: '1.8', fontWeight: '400' }], // 캡션
+      },
       colors: {
+        primary: '#EA0101',
         darkBrown: '#231F20',
         charcoal: '#35383E',
         lightGray: '#ECEEF2',
         slateGray: '#717781',
         turquoise: '#0098AF',
-        brightRed: '#EA0101',
         crimsonRed: '#D60C0C',
         fireEngineRed: '#F53232',
         deepBrown: '#272222',
