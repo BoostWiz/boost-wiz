cf7c1c88109e22fea5f624bbddabd155ae56e259
muffin9 <jinha6707@gmail.com>
2024-12-17 21:30:46 +0900

    feat: breadcrumb component 구현

---
+++ src/shared/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Breadcrumb } from '.';
+
+const meta = {
+  title: 'Breadcrumb',
+  component: Breadcrumb,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+  decorators: [(Story) => <Story />],
+  argTypes: {
+    breadList: {
+      control: 'object',
+      description: 'id, text, router로 이루어진 객체를 배열형태로 받습니다.',
+    },
+    pathname: {
+      control: 'text',
+      description:
+        '경로 pathname 통째로 받기 next/navgation 내의 usePathname 경로 그대로 넣어주시면 됩니다.',
+    },
+  },
+} satisfies Meta<typeof Breadcrumb>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    breadList: ['Game', '정규 리그', '경기 일정'],
+    pathname: '/game/regular/schedule',
+  },
+};
