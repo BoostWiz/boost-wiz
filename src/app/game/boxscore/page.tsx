c44ee2e1a39f3c60e3151ab363ddee5b7be98728
yeji <yeji11077@gmail.com>
2024-12-20 01:20:04 +0900

    feat: 박스스코어 페이지 생성 및 배너 배치

---
+++ src/app/game/boxscore/page.tsx
@@ -0,0 +1,14 @@
+import Banner from '@/shared/components/Banner';
+
+export default function BoxScorePage() {
+  return (
+    <section>
+      <div className="mt-[100px]">
+        <Banner
+          title="박스스코어"
+          description="박스스코어 정보를 알려드립니다."
+        />
+      </div>
+    </section>
+  );
+}
