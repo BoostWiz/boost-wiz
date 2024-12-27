d6873b259f0e611ada8fb32f74192081f985bd1e
muffin9 <jinha6707@gmail.com>
2024-12-27 13:32:00 +0900

    refactor: next.js 15버전에 맞게 코드 수정

--- src/app/api/players/[playerId]/route.ts
+++ src/app/api/players/[playerId]/route.ts
@@ -1,13 +1,19 @@
-// 자체 서버 열어서 테스트
 import { NextResponse } from 'next/server';
 import path from 'path';
 import fs from 'fs/promises';
+import { NextRequest } from 'next/server';
 
-export async function GET(
-  req: Request,
-  context: { params: { playerId: string } },
-) {
-  const { playerId } = await context.params;
+export async function GET(req: NextRequest) {
+  // URL에서 playerId를 추출
+  const url = new URL(req.url);
+  const playerId = url.pathname.split('/').pop();
+
+  if (!playerId) {
+    return NextResponse.json(
+      { error: 'Player ID is required' },
+      { status: 400 },
+    );
+  }
 
   try {
     const filePath = path.join(
