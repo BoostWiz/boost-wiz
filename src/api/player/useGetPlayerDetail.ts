c45d3888169ec342d99872b20c9ac263883e0969
muffin9 <jinha6707@gmail.com>
2024-12-30 15:25:35 +0900

    refactor: 불필요한 코드 삭제

--- src/api/player/useGetPlayerDetail.ts
+++ src/api/player/useGetPlayerDetail.ts
@@ -1,4 +1,4 @@
-import { PlayerDetailType, PlayerIdType } from '@/interface/player';
+import { PlayerDetailType } from '@/interface/player';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
@@ -9,12 +9,10 @@ const useGetPlayerDetail = (
   const { data } = useQuery({
     queryKey: ['players', pcode, playerDetailType],
     queryFn: async () => {
-      // TODO: server url 변경 예정
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/player/${playerDetailType}?pcode=${pcode}`,
       );
 
-      console.log(response);
       if (playerDetailType === 'coachdetail') {
         return response.data.data.coachstep;
       } else {
