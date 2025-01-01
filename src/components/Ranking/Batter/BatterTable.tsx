ca5f74b2f1fff3617e7d8efc25f82c2e50e7697a
yeji <yeji11077@gmail.com>
2025-01-02 05:35:15 +0900

    feat: ranking 페이지 투수순위, 타자순위 레이아웃

---
+++ src/components/Ranking/Batter/BatterTable.tsx
@@ -0,0 +1,5 @@
+const BatterTable = () => {
+  return <div>전체 타자 순위</div>;
+};
+
+export default BatterTable;
---
+++ src/components/Ranking/Batter/BatterTop5.tsx
@@ -0,0 +1,5 @@
+const BatterTop5 = () => {
+  return <div>전체 타자 top5</div>;
+};
+
+export default BatterTop5;
