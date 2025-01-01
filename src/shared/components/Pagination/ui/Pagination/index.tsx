0a01b1b4b0ff208980fa4d72d0e7d04aee675043
Laterre <laterre39@naver.com>
2025-01-01 14:32:00 +0900

    refactor: 페이지네이션 컴포넌트 리팩토링
    - 미디어 페이지 디자인에 맞게 일부 수정

--- src/shared/components/Pagination/ui/Pagination/index.tsx
+++ src/shared/components/Pagination/ui/Pagination/index.tsx
@@ -70,7 +70,7 @@ const PaginationPrevious = ({
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <span>이전</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -85,7 +85,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
+    <span>다음</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 )
@@ -101,7 +101,7 @@ const PaginationEllipsis = ({
     {...props}
   >
     <MoreHorizontal className="h-4 w-4" />
-    <span className="sr-only">More pages</span>
+    <span className="sr-only">더 많은 페이지</span>
   </span>
 )
 PaginationEllipsis.displayName = "PaginationEllipsis"
