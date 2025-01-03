aa0399ea72b474c6bdc1366331a75c7241032983
muffin9 <jinha6707@gmail.com>
2025-01-03 13:43:54 +0900

    refactor: next.config.ts 파일 수정

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  rewrites: async () => {
-    // console.log(process.env.NEXT_PUBLIC_API_URL);
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ];
-  },
+
   images: {
     domains: ['wizzap.ktwiz.co.kr', '54.180.228.165'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // 모든 도메인 허용
-      },
-    ],
   },
 };
 
