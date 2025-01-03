fe5b523e59c7fa3c3b68b75288640bfb50c38b57
muffin9 <jinha6707@gmail.com>
2025-01-03 14:39:27 +0900

    chore: http로 요청 보내지도록 rewrites로 해결하기

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
 
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 클라이언트가 호출할 경로
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
   images: {
     domains: ['wizzap.ktwiz.co.kr', '54.180.228.165'],
   },
