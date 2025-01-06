f33a29bfddbea41ebdc0478ae30e9d6f2d33ca33
Laterre <laterre39@naver.com>
2025-01-06 19:52:09 +0900

    refactor: wiz뉴스 페이지 스켈레톤 ui 개선

--- src/components/Media/NewsSkeleton.tsx
+++ src/components/Media/NewsSkeleton.tsx
@@ -8,16 +8,14 @@ const NewsSkeleton = ({count}: {count: number}) => {
         <div
           className={`${flexRow} h-[180px] gap-4 my-4 px-5 bg-[#ECEEF2] cursor-pointer rounded-lg shadow-lg shadow-slateGray`}>
           <div className={`${flexRowCenter} w-[23%] h-full`}>
-            <Skeleton className="w-[250px] h-[140px] rounded-lg shadow-xl" />
+            <Skeleton className="w-[250px] h-[140px] bg-[#D9DBE1] rounded-lg shadow-xl" />
           </div>
-          <div className={`${flexColumn} justify-center w-[75%] h-full gap-2`}>
-            <Skeleton className="w-full h-[25px] rounded-lg shadow-xl" />
-            <Skeleton className="w-full h-[45px] rounded-lg shadow-xl" />
+          <div className={`${flexColumn} justify-center w-[75%] h-full gap-4`}>
+            <Skeleton className="w-full h-[25px] bg-[#D9DBE1] rounded-lg shadow-xl" />
+            <Skeleton className="w-full h-[45px] bg-[#D9DBE1] rounded-lg shadow-xl" />
             <div className={`${flexRow} justify-end gap-2`}>
               <Skeleton
-                className={`${flexRowCenter} w-[85px] h-[20px] rounded-lg shadow-xl`} />
-              <Skeleton
-                className={`${flexRowCenter} w-[50px] h-[20px] rounded-lg shadow-xl`} />
+                className={`${flexRowCenter} w-[135px] h-[30px] bg-[#D9DBE1] rounded-lg shadow-xl`} />
             </div>
           </div>
         </div>
